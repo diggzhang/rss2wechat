414ed65579385986eced8466acbe7b532f423489
diggzhang <diggzhang@gmail.com>
2015-03-23 16:59:42 +0800

    shortly

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var wechat = require('wechat-enterprise');
 var API = require('wechat-enterprise').API;
-
-var app = express();
 
 var config = {
     token: 'hackthon',
     corpsecret: 'Yc4PJr25b3BYSE6kWNUYgYBW7l-ICBcNs9weUSdZPz6d-UMmRLo6cYFZRVpRXWl1',
     encodingAESKey: 'FpqVmn7klmiHYGgF6NrAxOPcHX13kHKumrfVuep1L6y',
     corpId: 'wx3412f502e6a35c42'
-  
 };
 
+var access_token = 'ZjeBxE9nFuAjGhTRoco1knrJlkmGHy40Doidm2taHChssPQUZ8fBtIr7sdQjBvr1';
 var to = {
-    "touser" : "diggzhang"
+    "touser" : "@all"
 };
 
 var message = {
+    "touser" : "@all",
     "msgtype": "text",
+    "agentid": "10",
     "text": {
         "content": "Holiday Request For Pony(http://xxxxx)"
     },
@@ -34,57 +26,13 @@ var message = {
 };
 
 
+var app = express();
+app.use(express.query());
 var api =  new API(config.corpId, config.corpsecret,'0');
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-app.use(express.query());
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 api.send(to, message, function (err, result) {
     console.log(err + result);
 });
-
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 module.exports = app;
